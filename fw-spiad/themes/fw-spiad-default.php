78898ba7850221532be365f123c8e1da444e0e49
fairweb <fairweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-09-20 07:20:02 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fw-subpageinadiv-spiad/tags/1.0@19347 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fw-spiad/themes/fw-spiad-default.php
@@ -0,0 +1,10 @@
+<div id="spiad">
+
+<!-- You may start editing here -->
+  <p>This is the default content.</p>
+  <p>It can be modified by editing the file located in plugin/fw-spiad/themes/fw-spiad-default.php.</p>
+  <p>Customize CSS in plugin/fw-spiad/themes/fw-spiad.css</p>
+  <p>This plugin was created by <a href="http://www.fairweb.fr" title="Fairweb, Wordpress developer">Fairweb</a> while working on the following project : <a href="http://www.navigation-web.com" title="Internet, donne-moi ce que je veux">www.navigation-web.com.</a></p>
+<!-- Stop editing -->
+
+</div>
